3fe890190c0b76fab026ffeb6d33e745b5b19eb0
Jeremy Walker <jez.walker@gmail.com>
2019-09-13 17:21:13 +0100

    Reorder fixture tests

--- test/test_runner_test.rb
+++ test/test_runner_test.rb
@@ -1,16 +1,6 @@
 require "test_helper"
 
 class TestRunnerTest < Minitest::Test
-  def test_exception
-    with_tmp_dir_for_fixture(:exception) do |path|
-      actual = JSON.parse(File.read(path / "report.json"))
-      assert_equal "error", actual["status"]
-
-      assert actual['error'].include?(%q{undefined local variable or method `raise_an_error_because_i_am_a_random_method' for main:Object (NameError)})
-      assert actual['error'].include?(%Q{\n\tfrom bin/run.rb:3:in `<main>'\n})
-    end
-  end
-
   def test_pass
     assert_fixture(:pass, {
       status: :pass,
@@ -35,4 +25,13 @@ class TestRunnerTest < Minitest::Test
     })
   end
 
+  def test_exception
+    with_tmp_dir_for_fixture(:exception) do |path|
+      actual = JSON.parse(File.read(path / "report.json"))
+      assert_equal "error", actual["status"]
+
+      assert actual['error'].include?(%q{undefined local variable or method `raise_an_error_because_i_am_a_random_method' for main:Object (NameError)})
+      assert actual['error'].include?(%Q{\n\tfrom bin/run.rb:3:in `<main>'\n})
+    end
+  end
 end
