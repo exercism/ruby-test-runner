d14d1e67b49727115de2f5eed87bfe35ad093c0e
Jeremy Walker <jez.walker@gmail.com>
2019-12-09 14:29:55 +0900

    Always return status 0 if the report gets written

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,14 @@
+# We want to override minitest's exit status
+# so we define this at the very top. We should
+# only override the exit status if we have a
+# report.json with some sensible things in.
+at_exit do
+  return unless MiniTest::ExercismReporter.instance
+  return unless MiniTest::ExercismReporter.instance.report_written?
+
+  exit 0
+end
+
 load File.expand_path('../../lib/test_runner.rb', __FILE__)
 
 TestRunner.run(ARGV[0], ARGV[1], ARGV[2])
