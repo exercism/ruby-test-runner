2f18c98c8a8b336dd75dce4e5ac55b57289c5a9f
Erik Schierboom <erik_schierboom@hotmail.com>
2020-08-27 09:08:57 +0200

    Use * as identifier

--- lib/minitest_ext/exercism_plugin.rb
+++ lib/minitest_ext/exercism_plugin.rb
@@ -1,7 +1,7 @@
 module Minitest
   def self.plugin_bogus_options(opts, options); end
 
-  def self.plugin_exercism_init(*_args)
+  def self.plugin_exercism_init(*)
     self.reporter.reporters.grep(Minitest::Reporter).each do |rep|
       rep.io = File.open(File::NULL, "w")
     end
